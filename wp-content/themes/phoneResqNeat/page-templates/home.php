38157a87498f8715a01439570a529eb213bf6238
Rohan Chambers <rohanchambers888@gmail.com>
2016-02-08 03:45:26 +0000

    creating and publishing template name

---
+++ wp-content/themes/phoneResqNeat/page-templates/home.php
@@ -0,0 +1,316 @@
+<?php
+/**
+ * Template Name: home
+ *
+ * This template home display content at full with, with no sidebars.
+ * Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site will use a different template.
+ *
+ * @package some_like_it_neat
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <article class="intro">
+                    <h1>Services<span></span></h1>
+                    <p>We’re here to serve you with all of your device-related needs. Whether your screen is cracked or you’re looking for assistance setting up a program on your computer, we’ve got you covered.</p>
+                    <p id="show-hide">Our services include, but are not limited to:<br>
+                    screen repair, speeding up your slow device, hard drive repair, jailbreaking, application and software setup assistance (Coming Soon), iCloud support and other miscellaneous services. Click below to discover more based on your needs.</p>
+                    <div id="mouse-scroll">
+                        <div class='icon-animate'></div>
+                        <i class="fa fa-angle-down fa-3x angle-down-1"></i>
+                        <i class="fa fa-angle-down fa-3x  angle-down-2"></i>
+                    </div>
+                    <hr />
+                </article>
+            </div><!-- End of row -->
+            
+            <!-- Repair cateogories -->
+            <div class="row row-products animatedParent" data-appear-top-offset="-100">
+                <article id="iphone-repair" class="repair animated fadeInLeft">
+                    <header>
+                        <span>iPhone repair</span>
+                        <h1>iPhone repair</h1>
+                        <p>Whether you have the original iPhone or the latest model, we can take a look at your device and figure out what steps need to be made to get you back on the grid.</p>
+                    </header>
+                    <a href="iphone-repair.php">&nbsp;</a>
+                </article>
+                <article id="ipad-repair" class="repair animated fadeInUp">
+                    <header>
+                        <span>iPad repair</span>
+                        <h1>iPad repair</h1>
+                        <p>If you don’t have the time to make an appointment and have your tablet sent away to a third-party repair shop, consider our in-house services for all of your iPad needs.</p>
+                    </header>
+                    <a href="ipad-repair.php">&nbsp;</a>
+                </article>
+                <article id="ipod-repair" class="repair animated fadeInRight">
+                    <header>
+                        <span>iPod repair</span>
+                        <h1>iPod Repair</h1>
+                        <p>So your iPod is busted and you need to get back to jamming out on your favorite tunes. We get it.</p>
+                    </header>
+                    <a href="ipod-repair.php">&nbsp;</a>
+                </article>
+            </div><!-- End of row -->
+
+            <div class="row animatedParent" data-appear-top-offset="-100">
+                <article id="android-repair" class="repair animated fadeInLeft">
+                    <header>
+                        <span>Android repair</span>
+                        <h1>Android Repair</h1>
+                        <p>We’re not just about Apple here. We work on Android devices as well. All devices and models are covered in the hands of our knowledgeable staff.</p>
+                    </header>
+                    <a href="android-repair.php">&nbsp;</a>
+                </article>
+                <article id="laptop-repair" class="repair animated fadeInUp">
+                    <header>
+                        <span>Laptop &amp; Desktop Repair</span>
+                        <h1>Laptop &amp; Desktop Repair</h1>
+                        <p>Doesn’t it feel like when your computer isn’t working, that life slows down? Let us take a look and we’ll get you up and running again in no time.</p>
+                    </header>
+                    <a href="laptop-repair.php">&nbsp;</a>
+                </article>
+                <article id="unlocks-repair" class="repair animated fadeInRight">
+                    <header>
+                        <span>Cell Phone unlocks</span>
+                        <h1>Cell Phone unlocks</h1>
+                        <p>Interested in unlocking your phone? We’ve got the tools to safely and securely unlock almost any smartphone device.</p>
+                    </header>
+                    <a href="unlock-repair.php"></a>
+                </article>
+            </div><!-- End of row -->
+        </div><!-- End of container -->
+
+        <div class="section-divider"></div>
+    </section><!-- End of Services Section -->
+
+
+    <section id="about">
+
+        <div class="container">
+            <div class="row">
+                <div class="bg-color bg-color-left"></div>
+                <div id="about-row-one" class="about-rows">
+                    <article>
+                        <header>
+                            <h1>Phone ResQ strives to create a new and unique experience for its customers:</h1>
+                            <p>A one-on-one interaction that redefines customer service geared toward consistency and ultimate satisfaction.</p>
+                            <p>We at Phone ResQ put our customers first and understand what it means to go a day or two without your device. When you put your device-related needs in our hands, we spring into action and work tirelessly to get your device running smoothly once again, so you can resume your life and work with ease.</p>
+                        </header>
+                    </article>      
+                </div>
+                <div id="about-row-one-img" class="about-row-img"></div>
+            </div><!-- End of row -->
+        
+            <div class="row">
+                <div class="bg-color bg-color-right"></div>
+                <div id="about-row-two" class="about-rows">
+                    <article class="left">
+                        <p>As Fernandina Beach’s premier Cell Phone Repair service center, we feel a responsibility in keeping prices low so that you don’t have to travel far to get the best price and service for your needs. We proudly carry strictly genuine parts that come at a low cost, which we can then pass those savings onto you.</p>
+                        <p>Don’t let the word “Phone” in our name confuse you— we don’t just do cell phones. We also work on and repair almost any device with a screen. Your computer, be it a laptop or desktop, cannot go weeks broken any more than your phone can!</p>
+
+                    </article>
+                </div>
+                <div id="about-row-two-img" class="about-row-img"></div>
+            </div><!-- End of row -->
+
+            <div class="row">
+                <div class="bg-color bg-color-left"></div>
+                <div id="about-row-three" class="about-rows">
+                    <article>
+                        <header>
+                            <p>The benefit to our services is that you’ll have all of your device-related needs taken care of under one roof! No need to go to different locations and scout multiple agents. We take care of all of your needs, in one place.</p>
+                            <p>If you’re looking for a quote or estimate on your desired repair, visit our <a href="#contact">Contact Us</a> section. Or stop by our shop during our regular business hours. No appointment needed. We can take a look at your device and let you know what steps will follow. Estimates are, of course, free and we will be able to diagnose the problem and what needs to be done on site. So there’s no need to send your device far and away for even the most complex of problems. It’s fast, easy and best of all: efficient.</p>
+                        </header>
+                    </article>      
+                </div>
+                <div id="about-row-three-img" class="about-row-img"></div>
+            </div><!-- End of row -->
+        </div><!-- End of container -->
+        <div class="section-divider"></div>
+
+    </section><!-- End of About -->
+
+
+    <section id="sell">
+        <div class="container">
+            <div class="row">
+                <div class="bg-color bg-color-right"></div>
+                <div id="sell-row" class="sell-rows">
+                    <article class="left">
+                        <header>
+                            <h1>Need to sell your iPhone, iPod, iPad, Mac?</h1>
+                            <p>No matter what the condition of your device is. We will buy it on the spot for cash.</p>
+                            <p>Please contact us for a quote! Use the contact form below or give us a call during business hours! 904-310-0059</p>
+                        </header>      
+                    </article>
+                </div>
+                <div id="sell-row-img" class="sell-row-img"></div>
+            </div><!-- End of row -->
+        </div>
+        <div class="section-divider"></div>
+
+    </section><!-- End of Sell -->
+
+    <section id="shop" class="">
+        <div class="container">
+            <div class="row">
+                <article class="intro">
+                    <header>
+                        <h1>Shop<span></span></h1>
+                        <p>Please find some products that are for sale or refurbished.</p>
+                    </header>
+                </article>
+            </div>
+
+            <div class="row">
+                <div id="shop-products" class="animatedParent" data-sequence="100">
+                    <article id="iPhone" class="product animated fadeInUp" data-id='1'>
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/shop/products/iphone.png" alt="iPhone repair" />
+                        <a href="#" class="btn-global btn-animate">Find out more</a>
+                    </article>
+
+                    <article id="samsung" class="product animated fadeInUp" data-id='2'>
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/shop/products/android.png" alt="iPad repair" />
+                        <a href="#" class="btn-global btn-animate">Find out more</a>
+                    </article>
+
+                    <article id="iPad" class="product animated fadeInUp" data-id='3'>
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/shop/products/ipad.png" alt="iPod repair" />
+                        <a href="#" class="btn-global btn-animate">Find out more</a>
+                    </article>      
+
+                    <article id="samsung-galaxy" class="product animated fadeInUp" data-id='4'>
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/shop/products/samsungTablet.png" alt="Samsung Galaxy repair" />
+                        <a href="#" class="btn-global btn-animate">Find out more</a>
+                    </article>
+
+                    <article id="accessories" class="product animated fadeInUp" data-id='5'>
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/shop/products/accessories.png" alt="Accessories" />
+                        <a href="#" class="btn-global btn-animate">Find out more</a>
+                    </article>
+                </div>            
+            </div><!-- End of row -->
+        </div><!-- End of Container -->
+
+    </section><!-- End of Shop -->
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <article class="intro">
+                    <header>
+                        <h1>Contact us<span></span></h1>
+                    </header>
+                    <?php
+                        //if "email" variable is filled out, send email
+                          if (isset($_REQUEST['email']))  {
+                          
+                          //Email information
+                          $admin_email = "rohanchambers888@gmail.com";
+                          $email = $_REQUEST['email'];
+                          $subject = "Phone Resq. message: " . $_REQUEST['subject'];
+                          $comment = $_REQUEST['comment'].$phone;
+                          $phone = $_REQUEST['phone'];
+                          $comment_phone = $comment . "\n\n Phone: ".$phone;
+
+                          //send email
+                          mail($admin_email, "$subject", "Comment: \n" . $comment_phone, "From:" . $email);
+                          
+                          //Email response
+                          echo "<p>Thank you, your message has been sent successfully! We will be in touch with your very soon.</p>";
+                          }
+                          
+                          //if "email" variable is not filled out, display the form
+                          else  {
+                        ?>
+
+                        <form id="contact-form" method="post" action="index.php#contact">
+                            <fieldset>
+                                <legend>Please fill in all the fields below to send us a message online.</legend>
+                                <p>
+                                    <input type="text" placeholder="Name" name="subject" id="resqFullname" required>
+                                    <span></span>
+                                    <label for="resqFullname"></label>
+                                </p>
+                                <p>
+                                    <input type="email" placeholder="Email" name="email" id="resqEmail" required>
+                                    <span></span>
+                                    <label for="resqEmail"></label>
+                                </p>
+                                <p>
+                                    <input type="numbers" placeholder="Phone number (optional)" name="phone" id="resqPhone">
+                                    <span></span>
+                                    <label for="resqPhone"></label>
+                                </p>        
+                                <p id="textarea">
+                                    <textarea placeholder="Your message" name="comment" id="resqMessage" required></textarea>
+                                    <span></span>
+                                    <label for="resqMessage"></label>
+                                </p>
+                                <p>
+                                    <button type="submit" id="btn-submit" class="btn-animate">Send</button>
+                                </p>
+                            </fieldset>       
+                        </form>
+                    <?php
+                      }
+                    ?>
+                </article>
+            </div>
+            
+            <div class="row"> 
+                <hr />  
+                <div id="google-map" class="two-col">
+                    <div id="map"></div>
+                </div>
+
+                <div id="get-in-touch" class="two-col">
+                    <article class="intro">
+     
+                            <h1>Get in touch<span></span></h1>
+                        
+                            <p>Contact us today! Stop by the store during normal business hours or call us at<br> 
+                            (904) 310-0059.</p>
+
+                            <p>We look forward to hearing from you!</p>
+
+                            <p>1001 Atlantic Avenue,<br> 
+                            Suite B,<br> 
+                            Fernandina Beach,<br>
+                            Florida, 32034</p>
+
+                            <p>Email: <a href="mailto:info@phoneresq.com">info@phoneresq.com</a><br>
+                            Hours: 10:00AM to 5:00 PM <br>
+                            (Monday-Saturday)</p>
+
+                    </article>    
+
+                </div>
+            </div>            
+        </div>
+        
+    </section><!-- End of contact -->
+
+    <section id="clients">
+        <div class="container">
+            <div class="row">
+                <ul id="client-list">
+                    <li class="client"><img src="<?php bloginfo('template_directory'); ?>/assets/images/clients/samsung.png" alt="Samsung" /></li>
+                    <li class="client"><img src="<?php bloginfo('template_directory'); ?>/assets/images/clients/apple.png" alt="Apple" /></li>
+                    <li class="client"><img src="<?php bloginfo('template_directory'); ?>/assets/images/clients/htc.png" alt="HTC" /></li>
+                    <li class="client"><img src="<?php bloginfo('template_directory'); ?>/assets/images/clients/android.png" alt="Android" /></li>
+                    <li class="client"><img src="<?php bloginfo('template_directory'); ?>/assets/images/clients/motorola.png" alt="Motorola" /></li>
+                    <li class="client"><img src="<?php bloginfo('template_directory'); ?>/assets/images/clients/blackberry.png" alt="Blackberry" /></li>
+                </ul>
+            </div>
+        </div>
+    </section><!-- End of clients -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/phoneResqNeat/page-templates/services.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Template Name: Services
+ *
+ * This template display content at full with, with no sidebars.
+ * Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site will use a different template.
+ *
+ * @package some_like_it_neat
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+
+<!-- iPhone repair -->
+<section id="repair">
+    <div class="container">
+
+        <div class="row row-intro">
+            <header>
+                <article class="intro">
+                    <h1>iPhone repair<span></span></h1>
+                    <p>Just about any problem with your iPhone can be repaired. Cracked screens, broken home buttons and charging ports, along with swapping the back of your device to a different color, and any micro-soldering work that can be done is very possible.</p> 
+                    <p>Bring your device in and we can fix it without having to send it away to a third-party service center. We do all of our repairs in-house, for your convenience and a speedy return. Check below for your device model and it’s price menu. We work with the iPhone 6, 6 PLUS, 5S, 5C, 5, 4S, 4, 3GS, and 3G.</p>
+                </article>
+            </header>
+        </div>
+
+        <div class="row">            
+            <div class="two-col">
+                <form id="filter-model">
+                    <div>
+                        <select class="filter-model btn-animate">
+                            <option value="reset">Filter model</option>
+                            <option value="iphone4">iPhone 4</option>
+                            <option value="iphone4s">iPhone 4s</option>
+                            <option value="iphone5">iPhone 5</option>
+                            <option value="iphone5c">iPhone 5s</option>
+                            <option value="iphone5c">iPhone 5c</option>
+                            <option value="iphone6">iPhone 6</option>
+                            <option value="iphone6s">iPhone 6s</option>
+                        </select>
+                        <i class="fa fa-caret-down fa-lg"></i>
+                    </div>
+
+                    <div>
+                        <select class="filter-model btn-animate" name="menu" onChange="top.location.href=this.options[this.selectedIndex].value;" value="GO">
+                            <option>Change brand</option>
+                            <option value="iphone-repair.php">iPhone</option>
+                            <option value="ipad-repair.php">iPad</option>
+                            <option value="ipod-repair.php">iPod</option>
+                            <option value="android-repair.php">Android</option>
+                            <option value="laptop-repair.php">Laptop</option>
+                            <option value="unlock-repair.php">Unlocks</option>
+                        </select>
+                        <i class="fa fa-caret-down fa-lg"></i>
+                    </div>
+                </form>         
+            </div>
+            <div class="two-col"></div>
+        </div>
+            
+        <div class="row-container">    
+            <div class="row animatedParent">
+                <div class="two-col filter iphone6s">
+                    <div class="img-holder animated fadeInLeft"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone6s.png" alt="iPhone6s" /></div>
+                    <h2>iPhone 6s and 6s plus</h2>
+                    <p>iPhone 6 Screen Replacement: $149.99<br>
+                    iPhone 6s+ Screen Replacement: $179.99<br>
+                    iPhone 6s and 6s+ Charging Port Replacement: $65<br>
+                    iPhone 6s and 6s+ Front Camera Replacement: $65<br>
+                    iPhone 6s and 6 Plus Rear Camera Replacement: $65<br>
+                    For more 6s and 6s+ repairs, please contact us!</p>
+                </div>
+                <div class="two-col filter iphone6">
+                    <div class="img-holder animated fadeInRight"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone6.png" alt="iPhone6" /></div>
+                    <h2>iPhone 6 and 6 plus</h2>
+                    <p><p>iPhone 6 Screen Replacement: $149.99<br>
+                    iPhone 6+ Screen Replacement: $179.99<br>
+                    iPhone 6 and 6+ Charging Port Replacement: $65<br>
+                    iPhone 6 and 6+ Front Camera Replacement: $65<br>
+                    iPhone 6 and 6 Plus Rear Camera Replacement: $65<br>
+                    For more 6 and 6+ repairs, please contact us!</p>
+                </div>
+            </div>
+
+            <div class="row animatedParent">
+                <div class="two-col filter iphone5s">
+                    <div class="img-holder animated fadeInLeft"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone5s.png" alt="iPhone5s" /></div>
+                    <h2>iPhone 5s</h2>
+                    <p>Cracked Screen Replacement: $110<br>
+                    Home Button: $65<br>
+                    Ear Speaker: $45<br>
+                    Front facing Camera: $45<br>
+                    Rear Facing Camera: $50<br>
+                    Loud speaker: $45<br>
+                    Charging Port: $65</p>
+                </div>
+                <div class="two-col filter iphone5c">
+                    <div class="img-holder animated fadeInRight"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone5c.png" alt="iPhone6c" /></div>
+                    <h2>iPhone 5c</h2>
+                    <p>Cracked Screen Replacement: $105<br>
+                    Home Button: $40<br>
+                    Ear Speaker: $45<br>
+                    Front Facing Camera: $45<br>
+                    Rear Facing Camera: $50<br>
+                    Large Speaker: $45<br>
+                    Charging Port: $60</p>
+                </div>
+            </div>   
+
+            <div class="row animatedParent">
+                <div class="two-col filter iphone5">
+                    <div class="img-holder animated fadeInLeft"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone5.png" alt="iPhone5" /></div>
+                    <h2>iPhone 5</h2>
+                    <p>Cracked Screen Replacement: $99<br>
+                    Home Button: $40<br>
+                    Ear Speaker: $45<br>
+                    Vibrate Motor: $45<br>
+                    Charging Port: $50</p>
+                </div>
+                <div class="two-col filter iphone4s">
+                    <div class="img-holder animated fadeInRight"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone4s.png" alt="iPhone4s" /></div>
+                    <h2>iPhone 4s</h2>
+                    <p>Cracked Screen Replacement: $65<br>
+                    Rear Cover Replacement: $35<br>
+                    Home Button: $45<br>
+                    Charging Port: $45<br>
+                    Color Swap Starting at: $80</p>
+                </div>
+            </div>
+
+            <div class="row animatedParent">
+                <div class="two-col filter iphone4">
+                    <div class="img-holder animated fadeInLeft"><img src="<?php bloginfo('template_directory'); ?>/assets/images/page-repair/iphone/iphone4.png" alt="iPhone4" /></div>
+                    <h2>iPhone 4</h2>
+                    <p>Cracked Screen Replacement: $65<br>
+                    Rear Cover Replacement: $35<br>
+                    Home Button: $45<br>
+                    Charging Port: $45<br>
+                    Color Swap Starting at: $80</p>
+                </div>
+            </div>
+
+        </div><!-- End of row container -->
+    </div>
+</div>    
+</section><!-- End of Repair section-->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
